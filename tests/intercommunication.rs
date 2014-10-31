81474ef9ba85740f08698ee3488a71984bd792e7
Alexey Fedorov <waterlink000@gmail.com>
2014-11-01 01:22:41 +0200

    fix #2: stop passing traits objects around: use parametric types

--- tests/intercommunication.rs
+++ tests/intercommunication.rs
@@ -3,17 +3,14 @@ extern crate raft_rs;
 
 mod using_dumb_network {
 
-    use std::io::timer::sleep;
-    use std::time::duration::Duration;
-
-    use raft_rs::intercommunication::{Intercommunication, DefaultIntercommunication, Package, Ack, Pack, start};
+    use raft_rs::intercommunication::{Intercommunication, DefaultIntercommunication, Ack, Pack, start};
 
     #[test]
     fn sending_simple_ack() {
         let mut comm: DefaultIntercommunication = Intercommunication::new();
 
-        let mut comm_1 = comm.register("host_1".to_string());
-        let mut comm_2 = comm.register("host_2".to_string());
+        let comm_1 = comm.register("host_1".to_string());
+        let comm_2 = comm.register("host_2".to_string());
 
         let stop_comm = start(comm);
 
@@ -24,7 +21,7 @@ mod using_dumb_network {
                 assert_eq!(from, "host_1".to_string());
                 assert_eq!(to, "host_2".to_string());
             },
-            _ => fail!("No ack")
+            _ => panic!("No ack")
         }
 
         stop_comm.send(0);
